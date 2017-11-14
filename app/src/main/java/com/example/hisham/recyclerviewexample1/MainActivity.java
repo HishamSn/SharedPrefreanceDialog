873f488ef75dcdbb6ee501678f6bbdfbd4553805
hisham <hsnaimeh@cloudconnectiv.com>
2017-11-14 16:50:50 +0200

    Initial commit

---
+++ app/src/main/java/com/example/hisham/recyclerviewexample1/MainActivity.java
@@ -0,0 +1,40 @@
+package com.example.hisham.recyclerviewexample1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    List<Student> studentList = new ArrayList<>();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        studentDataFillList();
+
+        initRecyclerView();
+
+
+    }
+
+    private void studentDataFillList() {
+        studentList.add(new Student("Hisham Jihad", "201110270", "25", "85"));
+        studentList.add(new Student("Malik abualzait", "200810223", "25", "93"));
+        studentList.add(new Student("Adnan", "200510270", "25", "93"));
+        studentList.add(new Student("Heba Jamal", "201110270", "25", "85"));
+    }
+
+    private void initRecyclerView() {
+        RecyclerView recyclerView = findViewById(R.id.recycler_activity_main);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setAdapter(new StudentAdapterList(studentList));
+    }
+}
---
+++ app/src/main/java/com/example/hisham/recyclerviewexample1/StudentAdapterList.java
@@ -0,0 +1,65 @@
+package com.example.hisham.recyclerviewexample1;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.List;
+
+/**
+ * Created by Hisham on 11/14/2017.
+ */
+
+public class StudentAdapterList extends RecyclerView.Adapter<StudentAdapterList.StudentViewHolder> {
+
+    private List<Student> studentList;
+
+    public StudentAdapterList() {
+    }
+
+    public StudentAdapterList(List<Student> studentList) {
+        this.studentList = studentList;
+    }
+
+    @Override
+    public StudentViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.row_student, parent, false);
+        return new StudentViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(StudentViewHolder holder, int position) {
+        final Student student= studentList.get(position);
+
+        holder.name.setText(student.getName());
+        holder.id.setText(student.getId());
+        holder.age.setText(student.getAge());
+        holder.avg.setText(student.getAvg());
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    class StudentViewHolder extends RecyclerView.ViewHolder {
+        TextView name;
+        TextView id;
+        TextView age;
+        TextView avg;
+
+        public StudentViewHolder(View itemView) {
+            super(itemView);
+
+            name = itemView.findViewById(R.id.textView_row_student_name);
+            id = itemView.findViewById(R.id.textView_row_student_id);
+            age = itemView.findViewById(R.id.textView_student_age);
+            avg = itemView.findViewById(R.id.textView_row_student_avg);
+        }
+
+
+    }
+}
