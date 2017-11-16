2459ffe0875272cd1351929ba2d8fd73410c5b3c
hisham <hsnaimeh@cloudconnectiv.com>
2017-11-16 17:01:09 +0200

    sql

--- app/src/main/java/com/example/hisham/recyclerviewexample1/models/Student.java
+++ app/src/main/java/com/example/hisham/recyclerviewexample1/models/Student.java
@@ -9,9 +9,8 @@ import java.io.Serializable;
  */
 
 public class Student implements Serializable{
-
-    private String name;
     private Integer id;
+    private String name;
     private Integer age;
     private Integer avg;
 
